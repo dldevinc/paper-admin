2ae95fe8586333e4671c3a923320415e7e9819eb
pix666 <pix666@ya.ru>
2021-04-16 15:18:11 +0400

    paper-pagination refactored

---
+++ paper_admin/static/paper_admin/src/bem/paper-pagination/paper-pagination.js
@@ -0,0 +1,33 @@
+import "./paper-pagination.scss";
+
+
+// Ctrl + Arrows navigation
+window.addEventListener("keydown", function(event) {
+    if (event.defaultPrevented) {
+        return; // Do nothing if the event was already processed
+    }
+
+    if (event.ctrlKey) {
+        const pagination = document.querySelector(".paper-pagination");
+        if (!pagination) {
+            return
+        }
+
+        switch (event.key) {
+            case "Left":
+            case "ArrowLeft":
+                pagination.querySelector(".page-link[aria-label=\"Previous\"]").click();
+                break;
+            case "Right":
+            case "ArrowRight":
+                pagination.querySelector(".page-link[aria-label=\"Next\"]").click();
+                break;
+            default:
+                // Quit when this doesn't handle the key event.
+                return;
+        }
+
+        // Cancel the default action to avoid it being handled twice
+        event.preventDefault();
+    }
+});
--- paper_admin/static/paper_admin/src/js/changelist.js
+++ paper_admin/static/paper_admin/src/js/changelist.js
@@ -4,40 +4,8 @@ import SortableTable from "js/components/sortable_table/SortableTable";
 import "js/components/actions";
 import "js/components/RelatedObjectLookups";
 import "bem/object-tools/object-tools";
+import "bem/paper-pagination/paper-pagination";
 import "bem/paper-search-form/paper-search-form";
-
-
-// Ctrl + Arrows navigation
-window.addEventListener("keydown", function(event) {
-    if (event.defaultPrevented) {
-        return; // Do nothing if the event was already processed
-    }
-
-    if (event.ctrlKey) {
-        const paperMain = document.getElementById("paper-main");
-        const pagination = paperMain.querySelector(".pagination");
-        if (!pagination) {
-            return
-        }
-
-        switch (event.key) {
-            case "Left":
-            case "ArrowLeft":
-                pagination.querySelector(".page-link[aria-label=\"Previous\"]").click();
-                break;
-            case "Right":
-            case "ArrowRight":
-                pagination.querySelector(".page-link[aria-label=\"Next\"]").click();
-                break;
-            default:
-                // Quit when this doesn't handle the key event.
-                return;
-        }
-
-        // Cancel the default action to avoid it being handled twice
-        event.preventDefault();
-    }
-});
 
 
 const table = document.getElementById("result_list");
