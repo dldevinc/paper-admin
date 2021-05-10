58c3e015e1de32ad19bede527da477750ad7f8f9
pix666 <pix666@ya.ru>
2021-05-10 17:22:53 +0400

    ip widget

--- paper_admin/static/paper_admin/src/js/app.js
+++ paper_admin/static/paper_admin/src/js/app.js
@@ -22,6 +22,7 @@ import "components/select2/select2";
 //  Widgets
 // ---------------
 import "widgets/date-field/date-field";
+import "widgets/ip-field/ip-field";
 import "widgets/email-field/email-field";
 import "widgets/number-field/number-field";
 import "widgets/password-field/password-field";
