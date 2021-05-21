926e2e116ad42bc0888aee0a7252da6f1592c02d
pix666 <pix666@ya.ru>
2021-05-21 23:13:38 +0400

    improve monkey-patching

--- paper_admin/monkey_patch.py
+++ paper_admin/monkey_patch.py
@@ -16,8 +16,8 @@ def get_original(cls=None, attr=None):
             def __init__(self, *args, **kwargs):
                 get_original()(*args, **kwargs)
     """
-    curent_frame = inspect.currentframe()
-    caller_frame = inspect.getouterframes(curent_frame, 2)
+    current_frame = inspect.currentframe()
+    caller_frame = inspect.getouterframes(current_frame, 2)
     caller_info = caller_frame[1]
 
     if isinstance(cls, str):
@@ -31,7 +31,17 @@ def get_original(cls=None, attr=None):
         cls = type(caller_info.frame.f_locals["self"])
 
     original_id = ORIGINAL_ID.format(cls.__name__, attr)
-    return getattr(cls, original_id)
+    original_methods = getattr(cls, original_id)
+
+    caller_filename = caller_info.frame.f_code.co_filename
+    caller_lineno = caller_info.frame.f_code.co_firstlineno
+    for index, method in enumerate(original_methods):
+        method_filename = method.__code__.co_filename
+        method_lineno = method.__code__.co_firstlineno
+        if method_filename == caller_filename and method_lineno == caller_lineno:
+            return original_methods[index + 1]
+    else:
+        return original_methods[0]
 
 
 class MonkeyPatchMeta(type):
@@ -56,14 +66,11 @@ class MonkeyPatchMeta(type):
 
             if hasattr(target, attr_name):
                 original_id = ORIGINAL_ID.format(target.__name__, attr_name)
-                if hasattr(target, original_id):
-                    raise RuntimeError(
-                        "An attribute named '{1}' already exists at the destination '{0}'".format(
-                            target.__name__,
-                            original_id
-                        )
-                    )
-                setattr(target, original_id, getattr(target, attr_name))
+                if not hasattr(target, original_id):
+                    setattr(target, original_id, [])
+
+                original_methods = getattr(target, original_id)
+                original_methods.insert(0, getattr(target, attr_name))
 
             setattr(target, attr_name, attr_value)
 
