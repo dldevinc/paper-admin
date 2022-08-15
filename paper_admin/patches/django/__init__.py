fc8368ae72ebe4a4dee986ee0eadd25177a747fd
pix666 <pix666@ya.ru>
2022-08-15 23:06:45 +0400

    fix isort issue

--- paper_admin/patches/django/__init__.py
+++ paper_admin/patches/django/__init__.py
@@ -1,16 +1,16 @@
 # Патч `renderer` должен вызываться после патча `widgets`, т.к. он ссылается
 # на файл admin.py приложения auth. Вызовы `admin.register()` должны выполняться
 # с уже пропатченным `formfield_overrides`.
-from . import (
+from . import (  # isort: skip
     changelist,
     filters,
     helpers,
     options,
     prepopulate,
-    renderer,
     sites,
     sortable,
     styles,
     tabs,
     widgets,
+    renderer,
 )
