ed63fc56d375e5e915e7bc7aa59ca6581856c0b9
pix666 <x896321475@gmail.com>
2023-12-12 15:04:31 +0400

    fix dropzone css classes

--- paper_admin/static/paper_admin/src/bem/paper-dropzone/paper-dropzone.js
+++ paper_admin/static/paper_admin/src/bem/paper-dropzone/paper-dropzone.js
@@ -1,12 +1,16 @@
 import "./paper-dropzone.scss";
 
+const OVERLAY_CLASS = "paper-dropzone__overlay";
+const OVERLAY_HIGHLIGHTED = "paper-dropzone__overlay--highlighted";
+
+
 document.addEventListener(
     "dragenter",
     event => {
         const dropzone = event.target.closest(".paper-dropzone");
         if (dropzone) {
-            const overlay = dropzone.querySelector(".paper-dropzone__overlay");
-            overlay && overlay.classList.add("paper-dropzone__overlay--highlighted");
+            const overlay = dropzone.querySelector(`.${OVERLAY_CLASS}`);
+            overlay && overlay.classList.add(OVERLAY_HIGHLIGHTED);
         }
     },
     true
@@ -17,8 +21,20 @@ document.addEventListener(
     event => {
         const dropzone = event.target.closest(".paper-dropzone");
         if (dropzone) {
-            const overlay = dropzone.querySelector(".paper-dropzone__overlay");
-            overlay && overlay.classList.remove("paper-dropzone__overlay--highlighted");
+            const overlay = dropzone.querySelector(`.${OVERLAY_CLASS}`);
+            overlay && overlay.classList.remove(OVERLAY_HIGHLIGHTED);
+        }
+    },
+    true
+);
+
+document.addEventListener(
+    "drop",
+    event => {
+        const dropzone = event.target.closest(".paper-dropzone");
+        if (dropzone) {
+            const overlay = dropzone.querySelector(`.${OVERLAY_CLASS}`);
+            overlay && overlay.classList.remove(OVERLAY_HIGHLIGHTED);
         }
     },
     true
